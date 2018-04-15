185a931be3ff779d0aba513871d6a0ff23a2a843
Liam Conrad <liam@storskegg.org>
2018-04-15 17:00:02 -0500

    populate src index

---
+++ src/index.js
@@ -0,0 +1,62 @@
+class Scryb {
+  constructor(props) {
+    const {
+      blacklist,
+      silentMode,
+      url,
+      whitelist
+    } = props;
+
+    if (silentMode !== undefined && typeof silentMode !== 'boolean') {
+      throw new Error('Option "silentMode" must be of type Boolean, if provided.');
+    }
+
+    if (whitelist !== undefined && !Array.isArray(whitelist)) {
+      throw new Error('Option "whitelist" must be of type Array, if provided.');
+    }
+
+    if (blacklist !== undefined && !Array.isArray(blacklist)) {
+      throw new Error('Option "blacklist" must be of type Array, if provided.');
+    }
+
+    if (url !== undefined) {
+      this._url = url;
+    } else {
+      this._url = false;
+    }
+
+    const cWhiteList = ['log', 'info', 'warn', 'error', 'debug'];
+    const cPropDescs = Object.getOwnPropertyDescriptors(window.console);
+
+    Object.keys(cPropDescs).forEach((key) => {
+      this[`_${key}`] = cWhiteList.includes(key) ? cPropDescs[key].value : this._noop;
+
+      if (cWhiteList.includes(key)) {
+        this[key] = function () {
+          const msg = JSON.stringify({
+            logType: key,
+            payloads: [...arguments]
+          });
+
+          if (this._url !== false) {
+            navigator.sendBeacon(this._url, new Blob([msg], { type: 'text/plain' }));
+          }
+
+          if (this.silentMode === false) {
+            this[`_${key}`](...arguments);
+          }
+        };
+      }
+    });
+
+    window.console = this;
+    Object.freeze(window.console);
+    Object.freeze(this);
+  }
+
+  _noop() {
+    this._log('Noop');
+  }
+}
+
+export default Scryb;
